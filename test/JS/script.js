35fe018eecb738af2288a2d0c574f4d35471934e
isa <chgpu_pi_20@mail.ru>
2024-12-02 16:12:53 +0300

    v2.0

--- test/JS/script.js
+++ test/JS/script.js
@@ -1,9 +1,12 @@
 let screenCounter = document.getElementById('screen-counter'),
+    audio = document.getElementById('audio'),
     playBtn = document.getElementsByClassName('btn-play')[0],
     countBtn = document.getElementsByClassName('btn-count')[0],
     resetBtn = document.getElementsByClassName('btn-reset')[0],
     btnRun = document.getElementsByClassName('btn-run')[0],
-    audio = document.getElementById('audio');
+    btnLeft = document.getElementsByClassName('btn-left')[0],
+    btnRight = document.getElementsByClassName('btn-right')[0];
+
 
 screenCounter.innerHTML = '0';
 
@@ -20,7 +23,61 @@ playBtn.addEventListener('click', function () {
 
     if (audio.paused) {
         audio.play();
+        screenCounter.innerHTML = 'Аль-Бакара - البقرة';
+        screenCounter.style.fontSize = '19px';
+        screenCounter.style.display = 'flex';
+        screenCounter.style.justifyContent = 'center';
+        screenCounter.style.alignItems = 'center';
+        countBtn.style.color = 'black';
+        resetBtn.style.color = 'black';
+
+        countBtn.disabled = true;
+        resetBtn.disabled = true;
+
     } else {
         audio.pause();
+        screenCounter.innerHTML = '0';
+        screenCounter.style = '';
+
+        countBtn.disabled = false;
+        resetBtn.disabled = false;
     }
+
+    const audioCounter = [
+        'mp3/audio.mp3',
+        'mp3/Sura_Lukman (mp3cut.net).mp3',
+        'mp3/Prekrasnoe_chtenie_Korana.mp3',
+        'mp3/Sura__Al_Mulk.mp3'
+    ];
+
+
+    const audioCounterText = [
+        'Аль-Бакар - البقرة',
+        'Сура Лукман - لقمان',
+        'Коран - القرآن',
+        'Сура Аль-Мульк - الملك'
+    ];
+
+    let a = 0;
+
+    function audioCounterSwitch(index) {
+        if (index >= 0 && index < audioCounter.length) {
+            a = index;
+            audio.src = audioCounter[a];
+            audio.play();
+            screenCounter.innerHTML = audioCounterText[a];
+        }
+    }
+
+    btnLeft.addEventListener('click', function () {
+        let newIndex = a - 1;
+        if (newIndex < 0) newIndex = audioCounter.length - 1;
+        audioCounterSwitch(newIndex);
+    });
+
+    btnRight.addEventListener('click', function () {
+        let newIndex = a + 1;
+        if (newIndex >= audioCounter.length) newIndex = 0;
+        audioCounterSwitch(newIndex);
+    });
 });
